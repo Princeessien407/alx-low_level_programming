80c80f6d4a9c4df7dda850a713ccce78cb1763e8
princeessien407 <princeessien407@gmail.com>
2022-10-18 10:48:05 -0700

    Betty checked commit 4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#incude  <stdio.h>
+
+/**
+ * main - prints the phrase with proper  grammer,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	print("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}
