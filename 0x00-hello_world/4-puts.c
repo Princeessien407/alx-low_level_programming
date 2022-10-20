0c3431b35509afef3330882de65e97d086392e30
princeessien407 <princeessien407@gmail.com>
2022-10-20 07:32:40 -0700

    fixed commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*main - "programming is like building a multilingual puzzle
+*main - Programming is like building a multilingual puzzle
 *Return: always (0)
 */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
