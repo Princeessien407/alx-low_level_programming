2dc7f13b2b4716cc5acba0efecc098f68962821d
princeessien407 <princeessien407@gmail.com>
2022-10-21 07:57:02 -0700

    fix commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
-*main - and that piece of art is useful - Dora Korpar, 2015-10-19
-*Return: 1 always
+*main - Entry point
+*Return: 0 always
 */
 
 int main(void)
+
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
-
