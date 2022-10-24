7e5e33e28dc198367d2b2a4e5419075fabcea206
princeessien407 <princeessien407@gmail.com>
2022-10-24 10:26:28 -0700

    fi

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,21 +1,22 @@
-#ifndef MAIN_H                                                                                                                                   
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
-#define MAIN_H                                                                                                                                   
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
+int _putchar(char c);
+int largest_number(int a, int b, int c, ...);
+int convert_day(int month, int day);
+void positive_or_negative(int);
+void print_remaining_days(int month, int day, int year);
+void print_number(int n);
+void print_line(int n);
+void print_square(int size);
+void print_triangle(int size);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_diagonal(int n);
+void print_numbers(void);
 
-                                                                                                                                                 
-
-#include <stdio.h>                                                                                                                               
-
-                                                                                                                                                 
-
-void positive_or_negative(int);                                                                                                                  
-
-int largest_number(int a, int b, int c, ...);                                                                                                    
-
-int convert_day(int month, int day);                                                                                                             
-
-void print_remaining_days(int month, int day, int year);                                                                                         
-
-                                                                                                                                                 
-
-#endif/* MAIN.H */                       
+#endif/* MAIN.H */
