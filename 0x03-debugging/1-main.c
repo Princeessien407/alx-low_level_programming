b2aa294ac59e3eb307cce72e7bf82cd034acc8e5
princeessien407 <princeessien407@gmail.com>
2022-10-24 05:31:40 -0700

    fix 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int i;
 
-	/*printf("infinite loop incoming : (\n");*/
+	/*printf("infinite loop incoming : (\n"); don't delete it*/
 
-	/*i = 0;*/
+	/*i = 0;don't delete it*/
+
+	/*while (i < 10)don't delete it*/	
 	
-	/*while (i < 10)*/	
-	
-	/*{*/
-		/*putchar(i);*/
-	/*}*/
+	/*{don't delete it*/
+		/*putchar(i);don't delete it*/
+	/*}don't delete it*/
 
 	printf("infinite loop avoided! \\o/\n");
 
