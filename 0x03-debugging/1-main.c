b4c45a67688d53f996743a40d66c051d0be2cb69
princeessien407 <princeessien407@gmail.com>
2022-10-24 05:39:05 -0700

    fix 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - don't delete the part that causes an infinite loop
+ * main - causes an infinite loop
  * Return: Always 0 (success)
  */
 
@@ -8,15 +8,15 @@ int main(void)
 {
 	int i;
 
-	/*printf("infinite loop incoming : (\n"); don't delete it*/
+	printf("infinite loop incoming : (\n"); /* don't delete it*/
 
-	/*i = 0;don't delete it*/
+	i = 0; /*don't delete it*/
 
-	/*while (i < 10)don't delete it*/	
+	while (i < 10) /*don't delete it*/	
 	
-	/*{don't delete it*/
-		/*putchar(i);don't delete it*/
-	/*}don't delete it*/
+	{ /*don't delete it*/
+		putchar(i); /*don't delete it*/
+	} /*don't delete it*/
 
 	printf("infinite loop avoided! \\o/\n");
 
