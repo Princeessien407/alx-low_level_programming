52b7c2a2c46a30756b2f214aa8fb59189a45a6b9
princeessien407 <princeessien407@gmail.com>
2022-10-24 11:10:30 -0700

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * main - prints the largest of 3 integers
  * Return: Always 0 (success)
  */
 
@@ -12,16 +11,5 @@ int main(void)
 
 	i = 98 % 2;
 	positive_or_negative(i);
-
-	int a, b, c;
-	int largest;
-
-	a = 972;
-	b = -98;
-	c = 0;
-
-	largest = largest_number(a, b, c);
-
-	printf("%d is the largest number\n", largest);
 	return (0);
 }
