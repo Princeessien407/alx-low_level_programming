a565f088e9b949b0218b18b30ccfced6bd9557d3
princeessien407 <princeessien407@gmail.com>
2022-10-25 10:06:46 -0700

    done fix

---
+++ 0x03-debugging/3-largest_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * largest_number - return the largest of 3 numbers
+ * @a: first interger
+ * @b: second interger
+ * @c: third interger
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest_number;
+
+	printf("Enter first number: %d", a);
+	scanf("%d", &a);
+	printf("Enter second number: %d", b);
+	scanf("%d", &b);
+	printf("Enter second number: %d", c);
+	scanf("%d", &c);
+
+	if (a > b && b > c)
+	return (largest);
+}
