13042fac03bafef3fa53decc026cd35af2052717
princeessien407 <princeessien407@gmail.com>
2022-10-27 09:32:19 -0700

    commit 0-last

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase letters
+ * _isdigit - checks for characters that are digits
  * @a: character to check
  *
  * Return: 0 or 1
  */
 
 
-int _isupper(int a)
+int _isdigit(int a)
 {
-	if (a >= 'A' && a <= 'Z')
+	if (a >= '0' && a <= '9')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character \ should be printed
+ * print_diagonal - prints a diagonal line of length n
+ * @n: length of diagonal
+ *
+ * Return: void
  */
+
+
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
-	
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
+	int a, b;
 
-			putchar('\n');
+	if (n <= 0)
+		_putchar('\n');
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
+
 }
