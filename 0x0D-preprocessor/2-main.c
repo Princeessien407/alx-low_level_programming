f2614b23bddcc97a704036e48d098b58a3a6bcee
princeessien407 <princeessien407@gmail.com>
2022-11-15 16:39:15 -0800

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints file name with new line
+ * main - prints file name it was compiled from with new line
  *
  * Return: Always 0.
  */
 int main(void)
 {
-printf("2-main.c 02-main.c\n");
+printf("2-main.c c\n");
 return (0);
 }
